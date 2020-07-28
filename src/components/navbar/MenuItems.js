be3178249e4efe169cd04c616652f3a70eeb2f95
lsmucassi <lindasmucassi@gmail.com>
2020-07-28 18:31:03 +0200

    let us nav style

---
+++ src/components/navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Who We Are',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Events',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Gallery',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'login',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import { MenuItems } from './MenuItems'
 
-export default function Navbar(){
-     
-    return (
-        <nav>
-            This is a Navbar
-        </nav>
-    )
+export default class Navbar extends Component {
+     render() {
+        return (
+            <nav className="NavbarItems" >
+                <h1 className="Navbar-logo">MY TASA<i className=""></i> </h1>
+                    <div className="menu-icon">
+
+                    </div>
+                <ul>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url} >
+                                    {item.title}    
+                                </a>
+                            </li>
+                        )
+                    })}
+
+                    
+                </ul>
+
+            </nav>
+        )     
+     }
 }
